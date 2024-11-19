6f4235b16cd7850aaad6757678c61c25c60fe550
Renato4qu1n0 <re4qu1n0@gmail.com>
2024-11-19 12:28:22 -0300

    Primeiro Commit do projeto 1.0

---
+++ src/main/java/web/huskyfinance/HuskyfinanceApplication.java
@@ -0,0 +1,13 @@
+package web.huskyfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuskyfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuskyfinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/huskyfinance/HuskyfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package web.huskyfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuskyfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
