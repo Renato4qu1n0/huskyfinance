b86bf3f799b46e451e03797c1d85f86510b2a333
Renato4qu1n0 <re4qu1n0@gmail.com>
2024-11-19 13:06:37 -0300

    1.1 Testes de página inicial

--- src/main/java/web/huskyfinance/HuskyfinanceApplication.java
+++ src/main/java/web/huskyfinance/HuskyfinanceApplication.java
@@ -2,11 +2,11 @@ package web.huskyfinance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class HuskyfinanceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HuskyfinanceApplication.class, args);
 	}
 
